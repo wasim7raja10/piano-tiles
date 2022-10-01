c5d763139dbe48ea4c49a47a3d5deb4ae461c225
Wasim Raja <wasim7raja10@gmail.com>
2022-10-01 19:25:02 +0530

    added data-index, hidden scroll on canvas

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 function makeCanvas() {
   const canvas = document.createElement("div");
   canvas.classList.add("canvas");
+  // canvas.classList.add("move");
   document.querySelector("body").appendChild(canvas);
   for (let i = 0; i < 2000; i++) {
     const rows = document.createElement("div");
     rows.classList.add("rows");
     rows.innerHTML = `
-        <div class="white-tiles"></div>
-        <div class="white-tiles"></div>
-        <div class="white-tiles"></div>
-        <div class="white-tiles"></div>
+        <div data-index='${i + "0"}' class="white-tiles"></div>
+        <div data-index='${i + "1"}' class="white-tiles"></div>
+        <div data-index='${i + "2"}' class="white-tiles"></div>
+        <div data-index='${i + "3"}' class="white-tiles"></div>
     `;
     const randPosition = Math.floor(Math.random() * 4);
     rows
@@ -20,3 +21,5 @@ function makeCanvas() {
 }
 
 makeCanvas();
+
+function detectClickOnTiles() {}
