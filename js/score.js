b547f5f40f637453b968da1d0a5fa1bcf9a707a3
Wasim Raja <wasim7raja10@gmail.com>
2022-10-02 15:08:41 +0530

    added local storage functionality

--- js/score.js
+++ js/score.js
@@ -1,5 +1,18 @@
-function updateScore(currentScore) {
-	return currentScore++;
+// update current score
+function updateScore(score) {
+	return score.currentScore++;
+}
+
+//checking for highest score
+function checkIfHighScore(score) {
+	if (score.currentScore > score.highScore) return true;
+	return false;
+}
+
+// UPDATING highScore
+function updateHighScore(score) {
+	window.localStorage.setItem("highScore", score.currentScore);
+	score.highScore = score.currentScore;
 }
 
 export { updateScore };
